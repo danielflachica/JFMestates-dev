f7dfd73120f852418025d777f9581467e840190e
Daniel Lachica <daniel.lachica82@gmail.com>
2018-05-23 23:35:59 +0800

    admin

---
+++ admin/login/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>JFM Estates | Login</title>
+	<?php require_once '../../res/templates/bootstrap-legal.php'; ?>
+	<?php require_once '../../res/templates/google-fonts.php'; ?>
+
+	<style type="text/css">
+		.bg-admin {
+			background: linear-gradient(-170deg, #607D8B, #263238);
+			color: #fff;
+		}
+	</style>
+</head>
+
+<body>
+
+	<div class="container-fluid p-lg-5 p-sm-4 bg-admin" style="height: 100vh">
+		<h1>JFM Estates &middot; Login</h1>
+	</div>
+	
+	<?php include '../../res/templates/footer-legal.php'; ?>
+</body>
+</html>
